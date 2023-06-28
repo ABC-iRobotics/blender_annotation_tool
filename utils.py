fed7e0b97a7959e7d123ddf659d1f04ef8aea031
Imi <91874793+nadasimre@users.noreply.github.com>
2023-06-28 13:23:28 +0200

    Added empty render and reset render engine

--- utils.py
+++ utils.py
@@ -68,20 +68,17 @@ def render_segmentation_masks(scene, instance_color_gen, self):
 
 def get_depth_image(scene):
 
-    # Step 1:
+    original_render_engine = scene.render.engine
     scene.render.engine = 'CYCLES'
 
-    # Step 2:
     scene.view_layers['ViewLayer'].use_pass_z = True
 
-    # Step 3:
     scene.use_nodes = True
     for node in scene.node_tree.nodes:
         scene.node_tree.nodes.remove(node)
     render_layers_node = scene.node_tree.nodes.new('CompositorNodeRLayers')
     viewer_node = scene.node_tree.nodes.new('CompositorNodeViewer')
     composite_node = scene.node_tree.nodes.new('CompositorNodeComposite')
-    normalize_node = scene.node_tree.nodes.new('CompositorNodeNormalize')
     link_viewer_render = scene.node_tree.links.new(render_layers_node.outputs["Depth"], viewer_node.inputs['Image'])
     link_composite_render = scene.node_tree.links.new(render_layers_node.outputs["Image"], composite_node.inputs['Image'])
 
@@ -101,56 +98,13 @@ def get_depth_image(scene):
         np.save(filepath, map)
 
     scene.render.filepath = original_filepath
-
-def get_depth_normalized_image(scene):
-
-    # Step 1:
-    scene.render.engine = 'CYCLES'
-
-    # Step 2:
-    scene.view_layers['ViewLayer'].use_pass_z = True
-
-    # Step 3:
-    scene.use_nodes = True
-    for node in scene.node_tree.nodes:
-        scene.node_tree.nodes.remove(node)
-    render_layers_node = scene.node_tree.nodes.new('CompositorNodeRLayers')
-    viewer_node = scene.node_tree.nodes.new('CompositorNodeViewer')
-    composite_node = scene.node_tree.nodes.new('CompositorNodeComposite')
-    normalize_node = scene.node_tree.nodes.new('CompositorNodeNormalize')
-    link_render_normalize = scene.node_tree.links.new(render_layers_node.outputs["Depth"], normalize_node.inputs['Value'])
-    link_viewer_normalize = scene.node_tree.links.new(normalize_node.outputs["Value"], viewer_node.inputs['Image'])
-    link_composite_render = scene.node_tree.links.new(render_layers_node.outputs["Image"], composite_node.inputs['Image'])
-
-    original_filepath = scene.render.filepath
-
-    if '.' in scene.render.filepath:
-        filepath = scene.render.filepath
-    else:
-        filepath = scene.render.frame_path(frame=scene.frame_current)
-    filepath = ''.join(filepath.split('.')[:-1]) + '_depth_map'
-    
-    scene.render.filepath = filepath
-
-    map = get_render_result()
-
-    if scene.bat_properties.save_annotation:
-        np.save(filepath, map)
-
-    scene.render.filepath = original_filepath
-
-# Step 4:
-def get_render_result():
-    viewer = bpy.data.images['Viewer Node']
-    w, h = viewer.size
-    img = np.array(viewer.pixels[:], dtype=np.float32)
-    img = np.reshape(img, (h, w, 4))[:,:,:]
-    img = np.flipud(img)
-    return img
+    scene.render.engine = original_render_engine
 
 def get_optical_flow(scene):
 
+    original_render_engine = scene.render.engine
     scene.render.engine = 'CYCLES'
+    
     scene.view_layers['ViewLayer'].use_pass_vector = True
 
     scene.use_nodes = True
@@ -178,10 +132,13 @@ def get_optical_flow(scene):
         np.save(filepath, map)
 
     scene.render.filepath = original_filepath
+    scene.render.engine = original_render_engine
 
 def get_surface_normal(scene):
 
+    original_render_engine = scene.render.engine
     scene.render.engine = 'CYCLES'
+    
     scene.view_layers['ViewLayer'].use_pass_normal = True
 
     scene.use_nodes = True
@@ -209,3 +166,15 @@ def get_surface_normal(scene):
         np.save(filepath, map)
 
     scene.render.filepath = original_filepath
+    scene.render.engine = original_render_engine
+
+def get_render_result():
+
+    bpy.ops.render.render(animation=False, write_still=False, use_viewport=False, layer="", scene="")
+
+    viewer = bpy.data.images['Viewer Node']
+    w, h = viewer.size
+    img = np.array(viewer.pixels[:], dtype=np.float32)
+    img = np.reshape(img, (h, w, 4))[:,:,:]
+    img = np.flipud(img)
+    return img
